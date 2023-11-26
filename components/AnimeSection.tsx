37ba2e52ab303b7c57556d0fd951de4516d98247
Anish Tiwari <87610940+AnishTiwari16@users.noreply.github.com>
2023-11-27 01:03:24 +0530

    added rtk + Inview intersection-observer (#2)

--- components/AnimeSection.tsx
+++ components/AnimeSection.tsx
@@ -2,18 +2,20 @@ import { fetchAnime } from '@/app/actions';
 import React from 'react';
 import AnimeCards from './AnimeCards';
 import { ApiDataResp } from '@/types';
+import LoadMore from './loadMore';
 
 const AnimeSection = async () => {
+    //as adding use state will call for client side and we are using server side for fetcing
     const data = await fetchAnime(1);
-    console.log(data);
     return (
         <div className="pt-5">
-            <div>Explore Animes</div>
+            <div className="font-semibold text-xl">Explore Animes</div>
             <div className="grid grid-cols-3 pt-5 gap-10">
                 {data?.map((elem: ApiDataResp, index: number) => (
                     <AnimeCards data={elem} index={index} />
                 ))}
             </div>
+            <LoadMore />
         </div>
     );
 };
